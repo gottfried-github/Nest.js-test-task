764f2e55a1195e6b1d07e58493369796174d03ed
osmomysl <dannvx@gmail.com>
2023-07-14 21:54:19 +0300

    use serve-static

--- src/app.module.js
+++ src/app.module.js
@@ -1,10 +1,19 @@
+import path from 'path'
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, '../public'),
+            serveStaticOptions: {
+                index: false
+            }
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
