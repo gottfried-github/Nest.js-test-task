1d1a7c53106e99a2fd783ad9b33ae47e0425302f
osmomysl <dannvx@gmail.com>
2023-07-16 00:03:35 +0300

    token schema: create index

--- src/signup/token.schema.js
+++ src/signup/token.schema.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const TokenSchema = new mongoose.Schema({
     token: {
         type: String,
-        required: true
+        required: true,
+        index: true
     }
 });
 
