eefbb08edf5d33a3abc688011853147a5bbc637a
osmomysl <dannvx@gmail.com>
2023-07-17 02:34:55 +0300

    front-end: use React

--- src/front-end/index.js
+++ src/front-end/index.js
@@ -1,2 +1,14 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+
+import App from './app.js'
+
 import './index.html'
 import './index.scss'
+
+function main() {
+    const root = ReactDOM.createRoot(document.querySelector('#main'))
+    root.render(<App />)
+}
+
+main()
